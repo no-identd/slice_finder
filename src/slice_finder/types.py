cd58af05615652178207dcbd7ecc233a0ea1617b
Igal Leikin <igaloly@gmail.com>
2023-04-07 08:59:58 +0300

    change print text

--- src/slice_finder/types.py
+++ src/slice_finder/types.py
@@ -25,10 +25,16 @@ class Extreme:
 
                 Returns:
                     Prettified text."""
-
-        return f"""Value of the metric on the whole dataset: {self.data_metric_value}
-Value of the metric on the filtered data: {self.filtered_data_metric_value}
-Filters: {self.filters}"""
+        
+        textual_filters = [
+            f'{filter.feature} {filter.operator} {filter.value}'
+            for filter in self.filters
+        ]
+        textual_composite_filter = '\nAND '.join(textual_filters)
+        return f"""When {textual_composite_filter},
+the value of the metric on the filtered data is {self.filtered_data_metric_value}
+compared to {self.data_metric_value} on the whole data.
+"""
 
     def __str__(self) -> str:
          return self.get_prettified_view()
